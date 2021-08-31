315c2c5f091ab3039791c8f5abec275c3fc6ba53
Christopher <chrisrw300@gmail.com>
2021-08-30 19:44:33 -0600

    more style, little js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
+// Array
+var activitiesList = ["wow", "wowie", "cats"];
+
 // Variables
+let btn = document.querySelector("#btn");
+
+// Print to Document
+function startPrint() {
+    alert('hello');
+}
+
+// Button 
+btn.addEventListener("click", startPrint);
